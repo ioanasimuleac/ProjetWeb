07c1ca01872b8023815731a5ef425067018f369b
Ioana Simuleac <ioana.simuleac@gmail.com>
2018-06-03 17:34:37 +0200

    modifications

--- config/database.php
+++ config/database.php
@@ -1,4 +1,10 @@
 <?php
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
@@ -12,6 +18,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    'fetch' => PDO::fetch_CLASS,
 
     'default' => env('DATABASE_URL', 'pgsql'),
 
@@ -55,18 +62,16 @@ return [
         ],
 
         'pgsql' => [
-            #'read' => ['host' => '127.0.0.1'],
-            #'write' => ['host' => '127.0.0.1'],
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
