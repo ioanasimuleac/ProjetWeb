2c530b4650ab82cc933a33cfac9e5693ade7e1a4
Ioana Simuleac <ioana.simuleac@gmail.com>
2018-06-05 18:43:36 +0200

    m

--- resources/views/indexVisitor.blade.php
+++ resources/views/indexVisitor.blade.php
@@ -89,91 +89,93 @@
             <!-- /.navbar-static-side -->
         </nav>
 
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">A propos de nous</h1>
+        <div class = "container">
+            <div id="page-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">A propos de nous</h1>
+                    </div>
+                    <!-- /.col-lg-12 -->
                 </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class = "col-lg-8">
-                    <p>
-                        Selon l'inventaire forestier national (2006-2011), la forêt en Roumanie couvre 17 millions d'hectares soit 170 000 km2. Cela correspond à un taux de boisement de 29,7% du territoire proche du taux mondial et qui est en progression. Elle comprend 136 espèces d'arbres.
-                    </p>
-                    <h3>Essences principales</h3>
-                    <hr>
-                    <p>
-                        En Roumanie, selon l'édition 2016 de l'inventaire de ROMSILVA, le volume sur pied est de 2,6 milliards de mètres cubes. Les feuillus en représentent 64% (1664 millions de mètres cubes) et les conifères 36% (936 millions de mètres cubes).
-                    </p>
-                    <p>
-                        Le volume se répartit entre:
-                    </p>
-                    <div>
-                        <div class="col-lg-6">
-                            <ul>
-                                <li>le chêne pédonculé, 11% ;</li>
-                                <li>le chêne rouvre, 11% ;</li>
-                                <li>le hêtre, 11% ;</li>
-                                <li>le chêne pubescent, 4% ;</li>
-                                <li>le charme, 4% ;</li>
-                                <li>le frêne, 4% ;</li>
-                                <li>le chêne vert, 1% ;</li>
-                            </ul> 
+                <!-- /.row -->
+                <div class="row">
+                    <div class = "col-lg-8">
+                        <p>
+                            Selon l'inventaire forestier national (2006-2011), la forêt en Roumanie couvre 17 millions d'hectares soit 170 000 km2. Cela correspond à un taux de boisement de 29,7% du territoire proche du taux mondial et qui est en progression. Elle comprend 136 espèces d'arbres.
+                        </p>
+                        <h3>Essences principales</h3>
+                        <hr>
+                        <p>
+                            En Roumanie, selon l'édition 2016 de l'inventaire de ROMSILVA, le volume sur pied est de 2,6 milliards de mètres cubes. Les feuillus en représentent 64% (1664 millions de mètres cubes) et les conifères 36% (936 millions de mètres cubes).
+                        </p>
+                        <p>
+                            Le volume se répartit entre:
+                        </p>
+                        <div>
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li>le chêne pédonculé, 11% ;</li>
+                                    <li>le chêne rouvre, 11% ;</li>
+                                    <li>le hêtre, 11% ;</li>
+                                    <li>le chêne pubescent, 4% ;</li>
+                                    <li>le charme, 4% ;</li>
+                                    <li>le frêne, 4% ;</li>
+                                    <li>le chêne vert, 1% ;</li>
+                                </ul> 
+                            </div>
+                            <div class="col-lg-6">
+                                <ul>
+                                    <li>les autres feuillus, 12% ;</li>
+                                    <li>l'épicéa commun, 8 %;</li>
+                                    <li>le sapin pectiné, 8 %;</li>
+                                    <li>le pin sylvestre, 6 %;</li>
+                                    <li>le pin maritime, 5 %;</li>
+                                    <li>le douglas, 4 %;</li>
+                                    <li>les autres conifères, 5 %.</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="col-lg-6">
+                        <h3>La propriété de la forêt en Roumanie</h3>
+                        <hr>
+                        <p>
+                            Les trois quarts de la forêt roumaine represente la propriété de l'État. 10% sont propriétés privées. 
+                            Ces forêts publiques sont gérées par l'Office national des forêts.
+                        </p>
+                        <h4>La forêt privée</h4>. 
+                        <p>
+                            La surface de la forêt public est 120 070 km2 (un peu plus de 12 millions d’ha), soit presque 3/4 de la surface forestière (75%), et 71% du volume sur pied (1,7 milliard de mètres cubes).
+                        </p>
+                        <h4>La forêt domaniale</h4>
+                        <p>
+                           Les forêts privées ont généralement des origines anciennes (forêts royales, de chasses et de rentes, du clergé, ou anciens domaines de chasse ou seigneuriaux, souvent héritées de biens saisis à l'époque de la Révolution). 
+                        </p>
+                        <h3>Notre structure:</h3>
+                        <hr>
+                        <div class = "col-lg-6">
+                            <p>Le chef: Dupont Paul</p>
+                            <p>La surface: 13508 ha</p>
+                            <p>L'accessibilité: 70 km</p>
+                            <p>Le relief: montagne</p>
+                        </div>
+                        <div class = "col-lg-6">
+                            <p>La composition:</p>
                             <ul>
-                                <li>les autres feuillus, 12% ;</li>
-                                <li>l'épicéa commun, 8 %;</li>
-                                <li>le sapin pectiné, 8 %;</li>
-                                <li>le pin sylvestre, 6 %;</li>
-                                <li>le pin maritime, 5 %;</li>
-                                <li>le douglas, 4 %;</li>
-                                <li>les autres conifères, 5 %.</li>
+                                <li>l'épicéa: 77% ;</li>
+                                <li>le sapin, 12% ;</li>
+                                <li>le hêtre, 10% ;</li>
+                                <li>les autres, 1%.</li>
                             </ul>
                         </div>
                     </div>
-                    <h3>La propriété de la forêt en Roumanie</h3>
-                    <hr>
-                    <p>
-                        Les trois quarts de la forêt roumaine represente la propriété de l'État. 10% sont propriétés privées. 
-                        Ces forêts publiques sont gérées par l'Office national des forêts.
-                    </p>
-                    <h4>La forêt privée</h4>. 
-                    <p>
-                        La surface de la forêt public est 120 070 km2 (un peu plus de 12 millions d’ha), soit presque 3/4 de la surface forestière (75%), et 71% du volume sur pied (1,7 milliard de mètres cubes).
-                    </p>
-                    <h4>La forêt domaniale</h4>
-                    <p>
-                       Les forêts privées ont généralement des origines anciennes (forêts royales, de chasses et de rentes, du clergé, ou anciens domaines de chasse ou seigneuriaux, souvent héritées de biens saisis à l'époque de la Révolution). 
-                    </p>
-                    <h3>Notre structure:</h3>
-                    <hr>
-                    <div class = "col-lg-6">
-                        <p>Le chef: Dupont Paul</p>
-                        <p>La surface: 13508 ha</p>
-                        <p>L'accessibilité: 70 km</p>
-                        <p>Le relief: montagne</p>
-                    </div>
-                    <div class = "col-lg-6">
-                        <p>La composition:</p>
-                        <ul>
-                            <li>l'épicéa: 77% ;</li>
-                            <li>le sapin, 12% ;</li>
-                            <li>le hêtre, 10% ;</li>
-                            <li>les autres, 1%.</li>
-                        </ul>
+                    <div class = "col-lg-4">
+                        <figure>
+                            <img src="https://www.google.fr/search?biw=1536&bih=734&tbm=isch&sa=1&ei=oqgWW-7FBsSHmwWMn50g&q=padurea+romaniei+satelit&oq=padurea+romaniei+satelit&gs_l=img.3...6166.7894.0.8063.8.8.0.0.0.0.115.717.7j1.8.0....0...1c.1.64.img..0.0.0....0.CYf2aqLsQb4#imgrc=EW5S-TZEX1griM:" class ="img-responsive" alt="Une image avec le pays vue de satellite!">
+                            <figcaption>Image satellitale (août 2002), la forêt apparaît (vert foncé) couvre en 2016 10 millions d’hectares soit 29% du territoire</figcaption>
+                        </figure>
                     </div>
                 </div>
-                <div class = "col-lg-4">
-                    <figure>
-                        <img src="https://www.google.fr/search?biw=1536&bih=734&tbm=isch&sa=1&ei=oqgWW-7FBsSHmwWMn50g&q=padurea+romaniei+satelit&oq=padurea+romaniei+satelit&gs_l=img.3...6166.7894.0.8063.8.8.0.0.0.0.115.717.7j1.8.0....0...1c.1.64.img..0.0.0....0.CYf2aqLsQb4#imgrc=EW5S-TZEX1griM:" class ="img-responsive" alt="Une image avec le pays vue de satellite!">
-                        <figcaption>Image satellitale (août 2002), la forêt apparaît (vert foncé) couvre en 2016 10 millions d’hectares soit 29% du territoire</figcaption>
-                    </figure>
-                </div>
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
         </div>
         <!-- /#page-wrapper -->
     </div>
